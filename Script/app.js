7157149c0c91054be83a8271db1369c85b696aee
Victory Ulasi <VictoryUlasi@protonmail.ch>
2026-03-27 10:02:53 -0500

    Removed makeshift background animation + Fixed some alignment issues + removed scroll featur when sidebar is 100vw added temporary about me section

--- Script/app.js
+++ Script/app.js
@@ -2,10 +2,16 @@
 function showSidebar() {
   const sideBar = document.querySelector(".sideBar");
   sideBar.style.display = "flex";
+
+  if (window.innerWidth <= 600) {
+    document.body.style.overflowY = "hidden";
+  }
 }
 
 // Hide the sidebar (called when close button is clicked)
 function hideSidebar() {
   const sideBar = document.querySelector(".sideBar");
   sideBar.style.display = "none";
+
+  document.body.style.overflowY = "visible";
 }
